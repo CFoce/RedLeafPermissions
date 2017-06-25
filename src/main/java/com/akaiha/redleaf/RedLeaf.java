f1376f9af9b5966c75dc0a032780bff234c643ab
CFoce <cfoce.dev@gmail.com>
2017-06-25 09:24:29 -0400

    Initial Commit

---
+++ src/main/java/com/akaiha/redleaf/RedLeaf.java
@@ -0,0 +1,10 @@
+package com.akaiha.redleaf;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class RedLeaf extends JavaPlugin {
+	
+	public void onEnable() {}
+	
+	public void onDisable() {}
+}
